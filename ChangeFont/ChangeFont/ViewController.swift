cdce45afcdc2153481eb3c4bc8bd43e643365f95
JamieMaple <jamiemaple007@gmail.com>
2018-06-13 01:29:08 +0800

    feat: add change font demo

--- ChangeFont/ChangeFont/ViewController.swift
+++ ChangeFont/ChangeFont/ViewController.swift
@@ -13,14 +13,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textArea.font = UIFont(name: fontNames[0], size: fontSize)
         changeButton.layer.cornerRadius = CGFloat(50)
     }
+    
+    let fontNames = [
+        "Gaspar_Regular",
+        "MFTongXin_Noncommercial-Regular",
+        "MFZhiHei_Noncommercial-Regular",
+        "MFJinHei_Noncommercial-Regular",
+    ]
+    
+    let fontSize = CGFloat(14)
 
     @IBOutlet weak var textArea: UITextView!
     @IBOutlet weak var changeButton: UIButton!
     
     @IBAction func changeFont(_ sender: UIButton) {
-        textArea.font = UIFont(name: "Medium", size: 20)
+        textArea.font = getFont()
+    }
+    
+    func getFont() -> UIFont? {
+        var fontName = ""
+        repeat {
+            let randomFontIndex = Int(arc4random_uniform(UInt32(fontNames.count)))
+            fontName = fontNames[randomFontIndex]
+        } while fontName == textArea.font?.fontName
+        
+
+        
+        return UIFont(name: fontName, size: fontSize)
     }
     
 }
